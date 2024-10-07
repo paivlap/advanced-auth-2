ef07816846f7d51750ddd55ff13e7e92400e6301
Glitch (quiver-daily-biology) <none>
2024-10-07 12:14:47 +0000

    🚌💨 Checkpoint ./connection.js:70176418/149

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 async function main(callback) {
-    const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
+    const URI = process.env.MongoURI('mongodb+srv://testuser41:<Kurtturuusu66>@cluster0.hgejp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');; // Declare MONGO_URI in your .env file
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
