9d1a14b04b9e64896c07bd8b53fc4677f6162487
Glitch (advanced-auth) <none>
2024-10-07 13:48:34 +0000

    🏎🍶 Checkpoint ./server.js:70176418/441

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const { ObjectID } = require("mongodb");
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -40,6 +41,16 @@ myDB(async (client) => {
     });
   });
 
+    passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }));
+  
   //save user id to a cookie
   passport.serializeUser((user, done) => {
     done(null, user._id);
